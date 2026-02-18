9aacd5e6222fc984f467b5159f48f0995c13dfc4
al13e68 <albertmartin4@depaul.edu.in>
2026-02-18 12:25:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import Gallery from "./pages/Gallery"
+import About from "./pages/About"
+
+function App() {
+  return (
+    <div>
+      <nav style={{ display: "flex", gap: "20px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/gallery">Gallery</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
